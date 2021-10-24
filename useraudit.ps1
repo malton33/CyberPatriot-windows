1626949d9c30392ae6ecab0a9a14fb3a41f8bf77
malton33 <44654353+malton33@users.noreply.github.com>
2021-10-24 01:45:16 -0400

    foreach test

--- useraudit.ps1
+++ useraudit.ps1
@@ -1,2 +1,7 @@
 #requires -version 4.0
 #requires -RunAsAdministrator
+$normalusers = Get-Content .\normalusers.txt
+$adminusers = Get-Content .\adminusers.txt
+foreach ($user in $normalusers){
+    Write-Host $user
+}
