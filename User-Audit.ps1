de31cb33d8c2657a41aeab8a4deb4d6f5b21a526
malton33 <44654353+malton33@users.noreply.github.com>
2021-10-26 18:32:45 -0400

    function stuff

--- User-Audit.ps1
+++ User-Audit.ps1
@@ -1,18 +1,22 @@
-#requires -version 4.0
-#requires -RunAsAdministrator
-
-$ListUsers = Get-Content .\normalusers.txt
-$ListAdmins = Get-Content .\adminusers.txt
-$AllowedUsers = $ListUsers -split " "
-$AllowedAdmins = $ListAdmins -split " "
-$AllMachineUsers = Get-LocalUser | Format-Table -HideTableHeader -property Name
-$AllAllowedUsers = $AllowedUsers + $AllowedAdmins
- foreach ($user in $allusers){
-    Get-LocalUser $user
-    if($?) {
-        New-LocalUser $user
+function Audit {
+    #requires -version 4.0
+    #requires -RunAsAdministrator
+    
+    $ListUsers = Get-Content .\normalusers.txt
+    $ListAdmins = Get-Content .\adminusers.txt
+    $AllowedUsers = $ListUsers -split " "
+    $AllowedAdmins = $ListAdmins -split " "
+    $AllMachineUsers = Get-LocalUser | Format-Table -HideTableHeader -property Name
+    $AllAllowedUsers = $AllowedUsers + $AllowedAdmins
+    foreach ($user in $AllAllowedUsers){
+        Get-LocalUser $user
+        if(!($?)) {
+            New-LocalUser $user
+        }
     }
-    else {
-        break
+    foreach ($user in $AllMachineUsers){
+        if(!($AllAllowedUsers.Contains($user))){
+            Remove-LocalUser $user
+        }
     }
-}
+    }
