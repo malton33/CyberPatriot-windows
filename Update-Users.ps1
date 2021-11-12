0d98f3985411359416c92ba39a9161d57bc58afc
malton33 <44654353+malton33@users.noreply.github.com>
2021-11-11 19:32:18 -0500

    Change file name

---
+++ Update-Users.ps1
@@ -0,0 +1,62 @@
+function Update-Users([string]$action) {
+    #requires -version 4.0
+    #requires -RunAsAdministrator
+	$ListUsers = Get-Content .\normalusers.txt
+    $ListAdmins = Get-Content .\adminusers.txt
+    $AllowedUsers = $ListUsers -split " "
+    $AllowedAdmins = $ListAdmins -split " "
+    $AllMachineUsers = Get-LocalUser | Format-Table -HideTableHeader -property Name
+    $AllAllowedUsers = $AllowedUsers + $AllowedAdmins
+
+    Write-Verbose "Running action " + $action
+    if ($action -eq "user" -Or $action -eq "all") {}
+    {
+        foreach ($user in $AllAllowedUsers)
+        {
+            Get-LocalUser $user
+            # if command does not succeed
+            if(!($?)) 
+            {
+                Write-Verbose "Creating user " + $user
+                New-LocalUser $user
+                Write-Verbose "Created user " + $user
+            }
+        }
+        foreach ($user in $AllMachineUsers)
+        {
+            # if user is not in all allowed users
+            if(!($AllAllowedUsers.Contains($user)))
+            {
+                Write-Verbose "Removing user " + $user
+                Remove-LocalUser $user
+                Write-Verbose "Removed user " + $user
+            }
+        }
+    
+    }
+	
+	if ($action -eq "password" -or $action -eq "all")
+	{
+		# need a better way to do this but not sure and it doesn't really matter does it?
+		$password = ConvertTo-SecureString "qwerty123QWERTY123$$$" -AsPlainText -Force
+		foreach ($user in $AllMachineUsers)
+		{
+			Write-Verbose "Setting password for " + $user
+			Set-LocalUser -Name "$user" -Password $password -PasswordNeverExpires false
+			Write-Verbose "Set password for " + $user
+		}
+	}
+	
+	if ($action -eq "admin" -or $action -eq "all")
+	{
+		foreach ($user in $AllMachineUsers)
+		{
+			Write-Verbose "Checking if " + $user + " is admin"
+			if ($user -in $AllowedAdmins)
+			{
+				#todo
+			}
+		}
+		
+    }
+}
