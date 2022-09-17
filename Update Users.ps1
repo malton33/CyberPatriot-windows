35e8cbaa1f7842d3182a4ac8237e8c7d3cfd1da8
malton33 <44654353+malton33@users.noreply.github.com>
2022-09-17 03:41:44 -0400

    test

--- Update Users.ps1
+++ Update Users.ps1
@@ -157,7 +157,7 @@ Param (
 					Write-Verbose "$user is not an admin"
 				}
 			}
-            #This entire section was written by copilot AI and it's trying to autocomplete my comments nowa
+            #This entire section was written by copilot AI and it's trying to autocomplete my comments nowaa
 		}
         Write-Host "Set admin permissions"
 	}
